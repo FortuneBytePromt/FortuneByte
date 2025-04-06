9eadf0307ab78341a82a9df39b542e33e689ae4a
FortuneBytePromt <zepoo777+fortunebyte@gmail.com>
2025-04-06 21:05:55 +0800

    Add login system

---
+++ main.js
@@ -0,0 +1,22 @@
+function emailLogin() {
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(() => window.location.href = 'dashboard.html')
+    .catch(err => alert(err.message));
+}
+
+function emailRegister() {
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then(() => window.location.href = 'dashboard.html')
+    .catch(err => alert(err.message));
+}
+
+function googleLogin() {
+  const provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider)
+    .then(() => window.location.href = 'dashboard.html')
+    .catch(err => alert(err.message));
+}
