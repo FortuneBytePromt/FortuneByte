2c06df50c766bb1e4ccdc03fdfdeb0ca561d6b2a
FortuneBytePromt <zepoo777+fortunebyte@gmail.com>
2025-04-06 22:38:32 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
-// Replace with your Firebase config
+// firebase-config.js
 const firebaseConfig = {
-	apiKey: "AIzaSyBL3wpZ474dkRn2xtPA6bH66FOlz9itrmk",
-	authDomain: "fortunebyte-ed969.firebaseapp.com",
-	projectId: "fortunebyte-ed969",
-	storageBucket: "fortunebyte-ed969.firebasestorage.app",
-	messagingSenderId: "1033648186140",
-	appId: "1:1033648186140:web:2fca6d027cb0357664d0d9",	
+  apiKey: "AIzaSyBL3wpZ474dkRn2xtPA6bH66FOlz9itrmk",
+  authDomain: "fortunebyte-ed969.firebaseapp.com",
+  projectId: "fortunebyte-ed969",
+  storageBucket: "fortunebyte-ed969.appspot.com", //
+  messagingSenderId: "1033648186140",
+  appId: "1:1033648186140:web:2fca6d027cb0357664d0d9"
 };
 
 firebase.initializeApp(firebaseConfig);
