265adfb99b24ae83ae7f1298ad03f08385aac0a1
FortuneBytePromt <zepoo777+fortunebyte@gmail.com>
2025-04-06 21:44:39 +0800

    Add Login Page

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function emailLogin() {
   const email = document.getElementById('email').value;
   const password = document.getElementById('password').value;
   firebase.auth().signInWithEmailAndPassword(email, password)
-    .then(() => window.location.href = 'test.html')
+    .then(() => window.location.href = 'index.html')
     .catch(err => alert(err.message));
 }
 
@@ -10,13 +10,13 @@ function emailRegister() {
   const email = document.getElementById('email').value;
   const password = document.getElementById('password').value;
   firebase.auth().createUserWithEmailAndPassword(email, password)
-    .then(() => window.location.href = 'test.html')
+    .then(() => window.location.href = 'index.html')
     .catch(err => alert(err.message));
 }
 
 function googleLogin() {
   const provider = new firebase.auth.GoogleAuthProvider();
   firebase.auth().signInWithPopup(provider)
-    .then(() => window.location.href = 'test.html')
+    .then(() => window.location.href = 'index.html')
     .catch(err => alert(err.message));
 }
